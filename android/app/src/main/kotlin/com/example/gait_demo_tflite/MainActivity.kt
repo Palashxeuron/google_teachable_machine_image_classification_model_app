9823254eeee244295bd38c614fcac2ea82f6cc20
palashsarate <palash.s@xeuron.ai>
2022-04-06 22:22:19 +0530

    initial

---
+++ android/app/src/main/kotlin/com/example/gait_demo_tflite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gait_demo_tflite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ readme.txt
@@ -0,0 +1,1 @@
+place your trained tflite google teachable machine image classification model's label.txt and model.tflite file in the assets folder
